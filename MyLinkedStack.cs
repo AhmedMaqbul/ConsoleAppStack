873f011d8202831e5a3fa56d0db81be38eb27fe3
AhmedMaqbul <ahmedmaqbul.ma@gmail.com>
2024-01-02 15:36:55 +0530

    Add project files.

---
+++ MyLinkedStack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppStackInLinkedListForm
+{
+    public class MyLinkedStack
+    {
+        public Node Head;
+
+        public bool IsEmpty()
+        { 
+            return Head == null; 
+        }
+
+        public void Push(int data) //insertion of new element
+        {
+            Node newNode = new Node(data);
+            if(IsEmpty())
+            {
+                Head = newNode;
+                return;
+            }
+            newNode.Next = Head;
+            Head = newNode;
+        }
+
+        public int Pop() //deletion of exiting element from top/peek
+        {
+            if(IsEmpty())
+                return -1;
+
+            int top = Head.Data;
+            Head = Head.Next;
+            return top;
+        }
+
+        public int Peek()
+        {
+            if (IsEmpty())
+                return -1;
+
+            return Head.Data;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppStackInLinkedListForm
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MyLinkedStack stack = new MyLinkedStack();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+
+            while(!stack.IsEmpty())
+            {
+                Console.WriteLine(stack.Peek());
+                stack.Pop();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
